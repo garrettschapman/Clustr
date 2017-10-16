a433bf3c6caad6d47f987e7b7bac8fa2fb17c03f
Foxbud <breadboardfox@gmail.com>
2017-10-16 16:19:44 -0400

    Removed unused JPanel1 class.

--- src/main/java/com/teamclustr/clustrapp/GUIFrameMain.java
+++ src/main/java/com/teamclustr/clustrapp/GUIFrameMain.java
@@ -27,23 +27,11 @@ public class GUIFrameMain extends javax.swing.JFrame {
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
-                jPanel1 = new javax.swing.JPanel();
                 jMenuBarMain = new javax.swing.JMenuBar();
                 jMenuFile = new javax.swing.JMenu();
                 jMenuEdit = new javax.swing.JMenu();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-                javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-                jPanel1.setLayout(jPanel1Layout);
-                jPanel1Layout.setHorizontalGroup(
-                        jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 400, Short.MAX_VALUE)
-                );
-                jPanel1Layout.setVerticalGroup(
-                        jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 279, Short.MAX_VALUE)
-                );
 
                 jMenuFile.setText("File");
                 jMenuBarMain.add(jMenuFile);
@@ -57,11 +45,11 @@ public class GUIFrameMain extends javax.swing.JFrame {
                 getContentPane().setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(0, 400, Short.MAX_VALUE)
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(0, 279, Short.MAX_VALUE)
                 );
 
                 pack();
@@ -106,6 +94,5 @@ public class GUIFrameMain extends javax.swing.JFrame {
         private javax.swing.JMenuBar jMenuBarMain;
         private javax.swing.JMenu jMenuEdit;
         private javax.swing.JMenu jMenuFile;
-        private javax.swing.JPanel jPanel1;
         // End of variables declaration//GEN-END:variables
 }
