6df439739f0ab25a6d7c4b41b8b2dd2c595e7919
Foxbud <breadboardfox@gmail.com>
2017-10-17 09:34:53 -0400

    Create all skeleton classes.

---
+++ src/main/java/com/teamclustr/clustrapp/communication/Post.java
@@ -0,0 +1,22 @@
+package com.teamclustr.clustrapp.communication;
+
+import java.io.Serializable;
+
+/**
+ * BRIEF CLASS DESCRIPTION.
+ * 
+ * @author Team Clustr
+ * @version 1.0
+ * File: Post.java
+ * Created: 10/17/2017
+ * Copyright (c) 2017, Team Clustr, All rights reserved.
+ * Summary of Modifications:
+ *  N/A
+ */
+public class Post implements Serializable {
+	
+	// MEMEBR DATA.
+	
+	// MEMBER METHODS.
+	
+}
