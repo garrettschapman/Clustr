bc5e3a1a3d7075b031a7c8810d7ef11517b43bf4
tybird13 <tylerdalbora@gmail.com>
2017-10-20 12:23:12 -0400

    Added logo to project

--- src/com/teamclustr/clustrapp/gui/GUIFrameMain.java
+++ src/com/teamclustr/clustrapp/gui/GUIFrameMain.java
@@ -471,15 +471,10 @@ public class GUIFrameMain extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4", "Title 5"
+                "Group Name", "Number Of Members", "Tags", "Categories", "Join"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
